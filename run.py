8e8f58bfffc47610d3a8d8022c3e09516c199d6d
e-elshaikh <elshaikh@mit.edu>
2025-07-09 18:02:03 -0400

    Update run.py

--- run.py
+++ run.py
@@ -3,31 +3,45 @@ from zt2rw import Zotero2Readwise
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("readwise_token")
-    parser.add_argument("zotero_key")
-    parser.add_argument("zotero_library_id")
-    parser.add_argument("--library_type", default="user")
-    parser.add_argument("--include_annotations", type=bool, default=True)
-    parser.add_argument("--include_notes", type=bool, default=False)
-    parser.add_argument("--filter_tags", nargs="*", default=None)
-    parser.add_argument("--include_filter_tags", action="store_true")
-    parser.add_argument("--use_since", action="store_true")
-    parser.add_argument("--suppress_failures", action="store_true")
+    parser.add_argument("readwise_token", help="Your Readwise API token")
+    parser.add_argument("zotero_key", help="Your Zotero API key")
+    parser.add_argument("zotero_library_id", help="Your Zotero library ID")
+    parser.add_argument(
+        "--library_type",
+        choices=["user", "group"],
+        default="user",
+        help="Zotero library type"
+    )
+    parser.add_argument(
+        "--include_annotations",
+        action="store_true",
+        default=True,
+        help="Include annotations"
+    )
+    parser.add_argument(
+        "--include_notes",
+        action="store_true",
+        default=False,
+        help="Include notes"
+    )
+    parser.add_argument(
+        "--suppress_failures",
+        action="store_true",
+        help="Continue on individual highlight failures"
+    )
+
     args = parser.parse_args()
 
-    zotero2readwise = Zotero2Readwise(
+    syncer = Zotero2Readwise(
         readwise_token=args.readwise_token,
         zotero_key=args.zotero_key,
         zotero_library_id=args.zotero_library_id,
         zotero_library_type=args.library_type,
         include_annotations=args.include_annotations,
         include_notes=args.include_notes,
-        filter_tags=args.filter_tags,
-        include_filter_tags=args.include_filter_tags,
-        use_since=args.use_since,
         suppress_failures=args.suppress_failures
     )
-    zotero2readwise.run()
+    syncer.run()
 
 if __name__ == "__main__":
     main()
